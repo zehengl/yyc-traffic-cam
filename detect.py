c4857c4f086faa6349034cf97318a5cb4f65a141
Zeheng Li <imzehengl@gmail.com>
2022-07-19 14:54:11 -0600

    add option to skip applying model

--- detect.py
+++ detect.py
@@ -29,6 +29,12 @@ parser.add_argument(
     help="export processed images in files",
 )
 parser.add_argument(
+    "--force",
+    action="store_true",
+    default=False,
+    help="force to re-detect",
+)
+parser.add_argument(
     "--batch_size",
     type=int,
     default=100,
@@ -48,6 +54,7 @@ parser.add_argument(
 )
 args = parser.parse_known_args()[0]
 save_runs = args.save_runs
+force = args.force
 batch_size = args.batch_size
 images_data = args.data
 output = args.output
@@ -57,25 +64,34 @@ output.mkdir(exist_ok=True)
 locations = list(Path(images_data).glob("*/"))
 
 for loc in tqdm(locations):
-    imgs = sorted(list(loc.glob("*.jpg")))
-    timestamps, counts = [], []
-    for batch_imgs in batch(imgs, batch_size):
-        results = model(batch_imgs)
+    summary_file = output / f"{loc.stem}.summary.csv"
 
-        if save_runs:
-            results.save()
+    if not summary_file.exists() or force:
+        imgs = sorted(list(loc.glob("*.jpg")))
+        timestamps, counts = [], []
+        for batch_imgs in batch(imgs, batch_size):
+            results = model(batch_imgs)
 
-        _timestamps = [f.split(".")[0].split("-")[1:] for f in results.files]
-        _timestamps = [datetime(*[int(i) for i in t]) for t in _timestamps]
-        _counts = [d.shape[0] for d in results.pandas().xyxy]
+            if save_runs:
+                results.save()
 
-        timestamps.extend(_timestamps)
-        counts.extend(_counts)
+            _timestamps = [f.split(".")[0].split("-")[1:] for f in results.files]
+            _timestamps = [datetime(*[int(i) for i in t]) for t in _timestamps]
+            _counts = [d.shape[0] for d in results.pandas().xyxy]
 
-    df = pd.DataFrame(dict(ds=timestamps, count=counts))
-    df["hour"] = df["ds"].dt.hour
-    df["weekday"] = df["ds"].dt.day_name()
+            timestamps.extend(_timestamps)
+            counts.extend(_counts)
 
+        df = pd.DataFrame(dict(ds=timestamps, count=counts))
+        df["hour"] = df["ds"].dt.hour
+        df["weekday"] = df["ds"].dt.day_name()
+
+        df.to_csv(
+            summary_file,
+            index=False,
+        )
+
+    df = pd.read_csv(summary_file)
     cname = loc / "cname.txt"
     if cname.exists():
         title = open(cname).read()
@@ -94,7 +110,21 @@ for loc in tqdm(locations):
     plt.close(fig)
 
     fig = plt.figure(figsize=(10, 6))
-    ax = sns.boxplot(data=df, x="weekday", y="count")
+    order = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+    ]
+    ax = sns.boxplot(
+        data=df,
+        x="weekday",
+        y="count",
+        order=[d for d in order if d in list(df["weekday"].unique())],
+    )
     if title:
         ax.set_title(title)
     fig.savefig(
@@ -103,11 +133,6 @@ for loc in tqdm(locations):
         bbox_inches="tight",
     )
     plt.close(fig)
-
-    df.to_csv(
-        output / f"{loc.stem}.summary.csv",
-        index=False,
-    )
 
 
 # %%
